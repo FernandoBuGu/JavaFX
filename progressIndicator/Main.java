1af3d10c7f2a05266d9bf404ab13f91db06d875b
Fernando Bueno Gutiérrez <fernando.buenogutierrez@wur.nl>
2020-05-20 06:17:46 -0600

    Add files via upload

---
+++ progressIndicator/Main.java
@@ -0,0 +1,43 @@
+package application;
+	
+/**
+ * JavaFX progress bar and progress indicator
+ * Example of unidirectional binding using JavaFX properties
+ * 
+ * @author feBueno - May 2020
+ * 
+ * REQUIRED:
+ * JavaFX - SceneBuilder setup
+ * REQUIRED IN SAME PACKAGE:
+ * TopGene.java
+ * Modal.java
+ * MainController.java
+ * Main.fxml
+ * applications.css
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
