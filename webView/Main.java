4a480630abf34d6c6e644e6913d418bdd1c79a61
Fernando Bueno Gutiérrez <fernando.buenogutierrez@wur.nl>
2020-05-22 04:27:36 -0600

    Update Main.java

--- webView/Main.java
+++ webView/Main.java
@@ -12,6 +12,12 @@ package application;
  * MainController.java
  * Main.fxml
  * applications.css
+ *
+ * If run in an IDE, the following may have to be added to VM arguments (i.e. in Eclipse in Run configurations):
+ *
+ * --module-path /usr/share/java/openjfx-11.0.2_linux-x64_bin-sdk/javafx-sdk-11.0.2/lib --add-modules=javafx.swing,javafx.graphics,javafx.fxml,javafx.media,javafx.web --add-reads javafx.graphics=ALL-UNNAMED --add-opens javafx.controls/com.sun.javafx.charts=ALL-UNNAMED --add-opens javafx.graphics/com.sun.javafx.iio=ALL-UNNAMED --add-opens javafx.graphics/com.sun.javafx.iio.common=ALL-UNNAMED --add-opens javafx.graphics/com.sun.javafx.css=ALL-UNNAMED --add-opens javafx.base/com.sun.javafx.runtime=ALL-UNNAMED
+ *
+ * where --module-path is the path to the Java lib foulder in the machine
  */
 
 import javafx.application.Application;
